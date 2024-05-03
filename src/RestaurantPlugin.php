3de4a976d1ca8862a54413d02834b3dd006bf515
Sorethea OK <sorethea@gmail.com>
2024-05-03 16:38:35 +0700

    add restaurant plugin

--- src/RestaurantPlugin.php
+++ src/RestaurantPlugin.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Sorethea\TheARestaurant;
+namespace Sorethea\Restaurant;
 
 use Filament\Contracts\Plugin;
 use Filament\Panel;
--- src/RestaurantServiceProvider.php
+++ src/RestaurantServiceProvider.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Sorethea\TheARestaurant;
+namespace Sorethea\Restaurant;
 
 use Illuminate\Support\ServiceProvider;
 use Spatie\LaravelPackageTools\Package;
