ffb616deaf848ca6940d6693d5b14c6d50b85fb1
Sorethea OK <sorethea@gmail.com>
2024-05-07 10:08:40 +0700

    update config

--- src/Factories/CuisineFactory.php
+++ src/Factories/CuisineFactory.php
@@ -2,7 +2,9 @@
 
 namespace Sorethea\Restaurant\Factories;
 
+use Illuminate\Database\Eloquent\Casts\Json;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\File;
 use Sorethea\Restaurant\Models\Cuisine;
 
 class CuisineFactory extends Factory
---
+++ src/Seeders/RestaurantSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Sorethea\Restaurant\Seeders;
+
+use Illuminate\Database\Seeder;
+use Sorethea\Restaurant\Models\Cuisine;
+
+class RestaurantSeeder extends Seeder
+{
+    public function run(): void
+    {
+        $cuisines = \File::json(__DIR__."/../cuisines.json");
+        foreach ($cuisines as $cuisine){
+            Cuisine::factory(1)->create([
+                "name"=>$cuisine->name,
+                "description"=>$cuisine->description,
+                "images"=>[$cuisine->image]
+            ]);
+        }
+    }
+}
