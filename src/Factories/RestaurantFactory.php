cf175b924dd1923f9f8c1b58642c6d967f787de7
Sorethea OK <sorethea@gmail.com>
2024-05-07 09:53:14 +0700

    update config

--- src/Factories/RestaurantFactory.php
+++ src/Factories/RestaurantFactory.php
@@ -11,16 +11,26 @@ class RestaurantFactory extends Factory
 {
     protected $model = Restaurant::class;
 
+
     public function definition(): array
     {
         $cuisines = Cuisine::all()->pluck('id','id')->toArray();
         return [
-            'name' => $this->faker->restaurant,
+            'name' => $this->generateRestaurantName(),
             'cuisine_id' => $this->faker->randomElement($cuisines),
             'logo' => $this->faker->imageUrl(),
             'image' => $this->faker->imageUrl(),
             'created_at' => Carbon::now(),
             'updated_at' => Carbon::now(),
         ];
+    }
+    function generateRestaurantName() {
+        $adjectives = ["Spicy", "Cozy", "Hidden", "Little", "Elegant"];
+        $nouns = ["Dragon", "Fork", "Spoon", "Kitchen", "Table"];
+
+        $adjective = $adjectives[array_rand($adjectives)];
+        $noun = $nouns[array_rand($nouns)];
+
+        return $adjective . " " . $noun;
     }
 }
