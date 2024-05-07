66b0afe5fb4eda4f416972ac6af4ab2fb6f56201
Sorethea OK <sorethea@gmail.com>
2024-05-07 11:40:36 +0700

    update cuisine

--- src/Seeders/RestaurantSeeder.php
+++ src/Seeders/RestaurantSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Sorethea\Restaurant\Seeders;
 
+use GuzzleHttp\Client;
 use Illuminate\Database\Seeder;
 use Sorethea\Restaurant\Models\Cuisine;
 
@@ -11,10 +12,19 @@ class RestaurantSeeder extends Seeder
     {
         $cuisines = \File::json(__DIR__."/../cuisines.json");
         foreach ($cuisines as $cuisine){
+            $file = file_get_contents($cuisine["image"]);
+
+            if(!empty($file)){
+                $finfo = new \finfo(FILEINFO_MIME_TYPE);
+                $fileInfo = $finfo->buffer($file);
+                $extension = pathinfo($fileInfo, PATHINFO_EXTENSION);
+                $fileName = uniqid().".".$extension;
+                \Storage::disk("local")->put($fileName,$file);
+            }
             Cuisine::factory(1)->create([
                 "name"=>$cuisine['name'],
                 "description"=>$cuisine['description'],
-                "images"=>[$cuisine['image']]
+                "images"=>[$fileName]
             ]);
         }
     }
