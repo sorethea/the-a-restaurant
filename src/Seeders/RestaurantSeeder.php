15f70ecc739eceed0bd3c1d8f1e77a2cff85e49a
Sorethea OK <sorethea@gmail.com>
2024-05-07 11:50:40 +0700

    update cuisine

--- src/Seeders/RestaurantSeeder.php
+++ src/Seeders/RestaurantSeeder.php
@@ -12,20 +12,25 @@ class RestaurantSeeder extends Seeder
     {
         $cuisines = \File::json(__DIR__."/../cuisines.json");
         foreach ($cuisines as $cuisine){
-            $file = file_get_contents($cuisine["image"]);
-            if ($file === false){
-                abort(404,"Image not found!");
+            try {
+                $file = file_get_contents($cuisine["image"]);
+                if ($file === false){
+                    abort(404,"Image not found!");
+                }
+                $finfo = new \finfo(FILEINFO_MIME_TYPE);
+                $fileInfo = $finfo->buffer($file);
+                $extension = pathinfo($fileInfo, PATHINFO_EXTENSION);
+                $fileName = uniqid().".".$extension;
+                \Storage::disk("local")->put($fileName,$file);
+                Cuisine::factory(1)->create([
+                    "name"=>$cuisine['name'],
+                    "description"=>$cuisine['description'],
+                    "images"=>[$fileName]
+                ]);
+            }catch (\Exception $exception){
+                logger($exception->getMessage());
             }
-            $finfo = new \finfo(FILEINFO_MIME_TYPE);
-            $fileInfo = $finfo->buffer($file);
-            $extension = pathinfo($fileInfo, PATHINFO_EXTENSION);
-            $fileName = uniqid().".".$extension;
-            \Storage::disk("local")->put($fileName,$file);
-            Cuisine::factory(1)->create([
-                "name"=>$cuisine['name'],
-                "description"=>$cuisine['description'],
-                "images"=>[$fileName]
-            ]);
+
         }
     }
 }
