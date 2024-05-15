77bba9fa722702abcb7f6253bffdc8b76bb7c5e7
Sorethea OK <sorethea@gmail.com>
2024-05-15 09:54:17 +0700

    update config

--- config/restaurant.php
+++ config/restaurant.php
@@ -4,5 +4,7 @@ return [
         "restaurant"=>"heroicon-o-building-office",
         "cuisine"=>"heroicon-o-bars-4",
         "category"=>"heroicon-o-list-bullet",
+        "price"=>"heroicon-o-currency-dollar",
+        "priceGroup"=>"heroicon-o-inbox-stack",
     ]
 ];
