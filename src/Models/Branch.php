1747fbbf0772d358c4f972104ad9c49b1f0c44fa
Sorethea OK <sorethea@gmail.com>
2024-06-21 11:15:45 +0700

    update

--- src/Models/Branch.php
+++ src/Models/Branch.php
@@ -4,6 +4,7 @@ namespace Sorethea\Restaurant\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Sorethea\Restaurant\Factories\BranchFactory;
 
 class Branch extends Model
@@ -13,5 +14,10 @@ class Branch extends Model
     protected static function newFactory(): BranchFactory
     {
         return new BranchFactory;
+    }
+
+    public function restaurant(): BelongsTo
+    {
+        return $this->belongsTo(Restaurant::class);
     }
 }
