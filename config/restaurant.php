3356256f91289e315f9a5217ba45b8dc22b29e85
Sorethea OK <sorethea@gmail.com>
2024-07-08 11:16:11 +0700

    restaurant plugin

--- config/restaurant.php
+++ config/restaurant.php
@@ -3,7 +3,7 @@ return [
     "icons"=>[
         "restaurant"=>"heroicon-o-building-office",
         "cuisine"=>"heroicon-o-bars-4",
-        "category"=>"heroicon-o-list-bullet",
+        "category"=>"gmti-category",
         "price"=>"heroicon-o-currency-dollar",
     ]
 ];
