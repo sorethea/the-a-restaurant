be3acb90db7e628883edb2fe2598eedc5e4e231d
Sorethea OK <sorethea@gmail.com>
2024-07-08 16:03:32 +0700

    restaurant module

--- src/PosPanelProvider.php
+++ src/PosPanelProvider.php
@@ -2,8 +2,18 @@
 
 namespace Sorethea\Restaurant;
 
+use Filament\Http\Middleware\Authenticate;
+use Filament\Http\Middleware\DisableBladeIconComponents;
+use Filament\Http\Middleware\DispatchServingFilamentEvent;
 use Filament\Panel;
 use Filament\PanelProvider;
+use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
+use Illuminate\Cookie\Middleware\EncryptCookies;
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
+use Illuminate\Routing\Middleware\SubstituteBindings;
+use Illuminate\Session\Middleware\AuthenticateSession;
+use Illuminate\Session\Middleware\StartSession;
+use Illuminate\View\Middleware\ShareErrorsFromSession;
 use Sorethea\Restaurant\Pages\PointOfSale;
 
 class PosPanelProvider extends PanelProvider
@@ -34,6 +44,22 @@ class PosPanelProvider extends PanelProvider
             ])
             ->pages([
                 PointOfSale::class
+            ])
+            ->middleware([
+                EncryptCookies::class,
+                AddQueuedCookiesToResponse::class,
+                StartSession::class,
+                AuthenticateSession::class,
+                ShareErrorsFromSession::class,
+                VerifyCsrfToken::class,
+                SubstituteBindings::class,
+                DisableBladeIconComponents::class,
+                DispatchServingFilamentEvent::class,
+            ])
+            ->authMiddleware([
+                Authenticate::class,
             ]);
     }
+
+
 }
